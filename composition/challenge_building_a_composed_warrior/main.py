740827bdc0cbaaa1eb65910987ad28fc358de2fd
katoriever <>
2026-05-25 15:51:52 +0000

    Challenge: Building a Composed Warrior

---
+++ composition/challenge_building_a_composed_warrior/main.py
@@ -0,0 +1,23 @@
+class Weapon:
+    def __init__(self, name, attack):
+        self.name = name
+        self.attack = attack
+
+class Warrior:
+    def __init__(self, name):
+        self.name = name
+
+    def equip_weapon(self, weapon):
+        self.weapon = weapon
+
+    def show_weapon(self):
+        if self.weapon == None:
+            print(f'{self.name} is unarmed.')
+        else:
+            print(f'{self.name} is holding a {self.weapon.name}')
+            
+sword = Weapon("Sword", 5)
+warrior = Warrior("Conan")
+
+warrior.equip_weapon(sword)
+warrior.show_weapon()
